373cf92ffa182cffea2f1812cc78b3a8cc6c43ee
infosys <infosys@C4204>
2025-04-21 09:58:02 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    //商品クラス
+    public class Product {
+        //商品コード
+        public int Code { get; set; }           //プロパティ
+        //商品名
+        public string Name { get; set; }
+        //商品価格（税抜き）
+        public int Price { get; set; }
+
+        
+
+        //消費税率は10%
+        private readonly double _taxRate = 0.1;  //フィールド
+                //↑読み取り専用
+
+        //消費税額を求める
+        public int GetTax() {                    //メソッド
+            return (int)(Price * _taxRate);
+        }
+
+        //コンストラクター
+        public  Product(int code, string name, int price) {
+            this.Code = code;
+            this.Name = name;
+            this.Price = price;
+
+        }
+
+        //税込み価格を求める
+        public int GetPriceIncludingTax() {
+            return Price + GetTax();
+        }
+    }
+}
---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(170, "大福", 250);
+
+
+            //税抜きの価格を表示【かりんとうの税抜き価格は○○円です】
+            Console.WriteLine( karinto.Name +"の税抜きは" + karinto.Price + "円です");
+            //消費税額の表示【かりんとうの消費税額は○○円です】
+            Console.WriteLine( karinto.Name+ "の消費税額は" + karinto.GetTax() + "円です" );
+            //税込み価格の表示【かりんとうの税込み価格は○○円です】
+            Console.WriteLine(karinto.Name + "の税込み価格は" + karinto.GetPriceIncludingTax() + "円です");
+
+            //大福バージョン
+            Console.WriteLine(daifuku.Name + "の税抜きは" + daifuku.Price + "円です");
+            Console.WriteLine(daifuku.Name + "消費税額は" + daifuku.GetTax() + "円です");
+            Console.WriteLine(daifuku.Name + "の税込み価格は" + daifuku.GetPriceIncludingTax() + "円です");
+
+
+
+
+        }
+    }
+}
+
+
