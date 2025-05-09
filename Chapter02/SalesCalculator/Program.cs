201496f613f89418d28a42c4300e672c965a8790
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-05-09 15:20:21 +0900

    コンストラクタの引数変更

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -2,7 +2,7 @@
 namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter (SalesCounter.ReadSales(@"data\sales.csv"));
+            SalesCounter sales = new SalesCounter (@"data\sales.csv");
 
             Dictionary<string, int> amountsPerStore = sales.GetPerStoreSales();
 
