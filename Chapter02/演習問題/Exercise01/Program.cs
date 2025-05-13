081127ef747e8daed8ad2d13a1af00458e592593
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-05-13 11:02:40 +0900

    2.1.3

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -2,7 +2,15 @@
     internal class Program {
         static void Main(string[] args) {
             //2.1.3
+            var Song = new Song[] {
+                new Song("Let it be", "The Beatles",243),
+                new Song("Bridge Over Troubled Water","Simon & Garfunkel",293),
+                new Song("Close To You","Carpenters",276),
+                new Song("Honesty","Billy Joel",231),
+                new Song("I Will Always Love You","Whitney Houston",273), 
+            };
 
+            
 
 
         }
