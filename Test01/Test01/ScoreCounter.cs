912b58b1477f3a482ad4729641e128911273d43a
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-06-17 12:12:25 +0900

    ファイル違うとこにありました ごめんなさい

---
+++ Test01/Test01/ScoreCounter.cs
@@ -0,0 +1,52 @@
+﻿namespace Test01 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
+
+        }
+
+        
+
+        //メソッドの概要： データを取得。List生成
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            var scores = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                string[] items = line.Split(',');
+                Student Scores = new Student() {
+                    //Name = items[0],
+                    //Subject = items[1],
+                    //Score = int.Parse(items[2]),
+                };
+                scores.Add(Scores);
+
+            }
+            
+            return scores;
+
+        }
+
+        //メソッドの概要： 科目別点数
+        public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+                //        if (dict.ContainsKey(score.Name))
+                //            dict[score.Name] += score.Score;
+
+                //        else
+
+                //            dict[score.Name] = score.Score;
+
+            }
+            return dict;
+        }
+
+
+        
+
+    }
+
+}
---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        String Name { get; } = String.Empty;
+        string Subject { get;} = String.Empty;
+        int Score { get; }
+
+
+    }
+}
