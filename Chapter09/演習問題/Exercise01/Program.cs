f0443ba65ef95ca479e66d32d66899d856ee7dc8
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-06-27 10:14:30 +0900

    9章Exercise01完成

--- Chapter09/演習問題/Exercise01/Program.cs
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -4,12 +4,15 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
+            #region メイン
             var dateTime = DateTime.Now;
             DisplayDatePattern1(dateTime);
             DisplayDatePattern2(dateTime);
             DisplayDatePattern3(dateTime);
         }
+        #endregion
 
+        #region 1番
         private static void DisplayDatePattern1(DateTime dateTime) {
             //2024/03/09 19:03
             //string.Formatを使った例
@@ -17,7 +20,9 @@ namespace Exercise01 {
             var hyouzi2 = string.Format("{0:yyyy/MM/dd HH:mm}", date);
             Console.WriteLine(hyouzi2);
         }
+        #endregion
 
+        #region 2番
         private static void DisplayDatePattern2(DateTime dateTime) {
             //2024年03月09日　19時03分09秒
             //DateTime.ToStringを使った例
@@ -25,10 +30,10 @@ namespace Exercise01 {
             var date = new DateTime(2024, 03, 09, 19, 03, 09);
             var hyouzi1 = date.ToString("yyyy年mm月dd日 HH時mm分ss秒");
             Console.WriteLine(hyouzi1);
-
-
         }
+        #endregion
 
+        #region 3番
         private static void DisplayDatePattern3(DateTime dateTime) {
             var culture = new CultureInfo("ja-JP");
             culture.DateTimeFormat.Calendar = new JapaneseCalendar();
@@ -44,5 +49,7 @@ namespace Exercise01 {
             Console.Write(hyouzi1);
             Console.WriteLine($"({dayofweek})");
         }
+        #endregion
+
     }
 }
