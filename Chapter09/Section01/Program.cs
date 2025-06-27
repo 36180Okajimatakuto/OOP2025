1e82473f902c1749d9e5e2917d7af7163637f13c
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-06-27 10:04:42 +0900

    DisplayDatePattern1,2完成

--- Chapter09/Section01/Program.cs
+++ Chapter09/Section01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
 using System.Globalization;
 
 namespace Section01 {
@@ -26,19 +27,52 @@ namespace Section01 {
             Console.Write("日:");
             int day =   int.Parse(Console.ReadLine());
             var birthday = new DateTime(year, month, day);
-            Console.WriteLine($"平成{year}年{month}月{day}日は{birthday:dddd}です");
+            Console.WriteLine($"{year}年{month}月{day}日は{birthday:dddd}です");
             #endregion
+
+            #region//お手本 日本語化
+            //var culture = new CultureInfo("ja-JP");
+            //culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            //var str = birth.ToString("ggyy年m月d日", culture);
+            //var shortDayOfWeek = culture.DayTimeFormat.GetshortestDayName(birth.DayOfWeek);
+            #endregion
+
             #region 日本語化
             //var culture = new CultureInfo("ja-JP");
             //culture.DateTimeFormat.Calendar = new JapaneseCalendar();
-            //string japaneseDate = birthday.ToString("ggyy年M月d日", culture);
+            //var str = birthday.ToString("ggyy年M月d日", culture);
+            //Console.WriteLine(str);
             #endregion
 
-            //②自分の生年月日はうるう年かをプログラムで書いて調べる
-            var uruuYear = new DateTime(2005, 11, 5);
+            #region//②自分の生年月日はうるう年かをプログラムで書いて調べる
+            var uruuYear = DateTime.IsLeapYear(year);
+            if (uruuYear) {
+                Console.WriteLine("閏年です。");
+            } else {
+                Console.WriteLine("閏年ではありません。");
+            }
+            #endregion
 
-            Console.WriteLine($"うるう年は、{uruuYear}");
-        
+            #region//③生まれてから〇〇〇〇日目です
+            TimeSpan nannititatta = DateTime.Now - birthday ;
+            Console.WriteLine($"入力された日から今日までに、{nannititatta.TotalDays}日目です。");
+            #endregion
+
+            #region//④あなたは〇〇歳です！
+            var tosi = today.Year - birthday.Year;
+            if (today < birthday.AddYears(tosi)) {
+                tosi--;//chatGPTのIF文
+            }
+            Console.WriteLine("あなたは" +tosi + "歳です。");
+            #endregion
+
+            #region//⑤1月1日から何日目か？
+            var todays = DateTime.Today;
+            int hinitikeisan = todays.DayOfYear;//おてほん
+            Console.WriteLine(hinitikeisan);
+            #endregion
+
+
         }
     }
 }
---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            //2024/03/09 19:03
+            //string.Formatを使った例
+            var date = new DateTime(2024, 03, 09, 19, 03, 09);//GPT使用
+            var hyouzi2 = string.Format("{0:yyyy/MM/dd HH:mm}", date);
+            Console.WriteLine(hyouzi2);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            //2024年03月09日　19時03分09秒
+            //DateTime.ToStringを使った例
+
+            var date = new DateTime(2024, 03, 09, 19, 03, 09);
+            var hyouzi1 = date.ToString("yyyy年mm月dd日 HH時mm分ss秒");
+            Console.WriteLine(hyouzi1);
+
+
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            int nitizi = int.Parse(Console.ReadLine());
+            var nyuuryoku = new DateTime(nitizi);
+            var dayofweek = culture.DateTimeFormat.GetDayName(nyuuryoku.DayOfWeek);
+            Console.WriteLine(dayofweek);
+        }
+    }
+}
