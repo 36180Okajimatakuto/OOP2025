c42829be78f85ada74251612e082dd1366d456d0
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-07-15 14:22:36 +0900

    12.1.1完成

--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using System.Text.Unicode;
 using System.Text.Encodings.Web;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace Exercise01 {
@@ -40,43 +41,56 @@ namespace Exercise01 {
 
         }
       
-
+        //問題12.1.1
         static string Serialize(Employee emp) {
             var options = new JsonSerializerOptions {
                 WriteIndented = true,
                 Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
-            
+            string jsonString = JsonSerializer.Serialize(emp, options);
             return JsonSerializer.Serialize(emp, options);
  
 
         }
 
         static Employee? Deserialize(string text) {
-
-            return JsonSerializer.Deserialize<Employee>(text); 
+            var options = new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            return JsonSerializer.Deserialize<Employee>(text,options); 
         }
 
         //問題12.1.2
         //シリアル化してファイルへ出力する
+        
         static void Serialize(string filePath, IEnumerable<Employee> employees) {
+            var options = new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
 
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            };
+            Byte[] utf8Bytes = JsonSerializer.SerializeToUtf8Bytes(employees, options);  
+            File.WriteAllBytes(filePath, utf8Bytes);
         }
 
         //問題12.1.3
-        //シリアル化してファイルへ出力する
+        //逆シリアル化してファイルを読み込む
         static Employee[] Deserialize_f(string filePath) {
             try {
                 string jsonString = File.ReadAllText(filePath);
 
-                Employee[] employees = JsonSerializer.Deserialize<Employee[]>(jsonString);
+                var options = new JsonSerializerOptions {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                };
+                Employee[]? employees = JsonSerializer.Deserialize<Employee[]>(jsonString, options);
 
-                return employees;
+                return employees ?? Array.Empty<Employee>();
             }
             catch (Exception ex) {
                 Console.WriteLine($"ファイルの読み込みまたはデシリアライズに失敗しました: {ex.Message}");
-                return Array.Empty<Employee>(); 
+                return Array.Empty<Employee>();
             }
         }
 
