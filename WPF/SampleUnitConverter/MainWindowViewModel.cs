efc8accb41d0b4f9fdff0f1e93532c56437aee1d
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-10-09 15:16:11 +0900

    Prism変更前

---
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitConverter {
+    class MainWindowViewModel : BindableBase {
+        //フィールド
+        private double metricValue;
+        private double imperialValue;
+
+        //▲で呼ばれるコマンド
+        public DelegateCommand ImperialUnitToMetric { get; private set; }
+        //▼で呼ばれるコマンド
+        public DelegateCommand MetricToImperialUnit { get; private set; }
+
+        //上のコンボボックスで選択されている値
+        public MetricUnit CurrentMetricUnit { get; set; }
+
+        //下のコンボボックスで選択されている値
+        public ImperialUnit CurrentImperialUnit { get; set; }
+
+
+        //プロパティ
+        public double MetricValue {
+            get => metricValue;
+            set => SetProperty(ref metricValue, value);
+            
+        }
+
+        public double ImperialValue {
+            get => imperialValue;
+            set => SetProperty(ref imperialValue ,value);
+            
+        }
+
+        public MainWindowViewModel() {
+            CurrentMetricUnit = MetricUnit.Units.First();
+            CurrentImperialUnit = ImperialUnit.Units.First();
+
+
+            ImperialUnitToMetric = new DelegateCommand(
+                () => MetricValue = CurrentMetricUnit.FormImperialUnit(
+                    CurrentImperialUnit,ImperialValue));
+
+            MetricToImperialUnit = new DelegateCommand(
+                () => ImperialValue = CurrentImperialUnit.FormMetricUnit(
+                    CurrentMetricUnit, MetricValue));
+        }
+
+    }
+}
---
+++ WPF/SampleUnitConverter/MetricUnit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    class MetricUnit :DistanceUnit {
+        private static List<MetricUnit> units = new List<MetricUnit> {
+            new MetricUnit{ Name = "mm",Coefficent = 1,},
+            new MetricUnit{ Name = "cm",Coefficent = 10,},
+            new MetricUnit{ Name = "m",Coefficent = 10 * 100,},
+            new MetricUnit{ Name = "Km",Coefficent = 10 * 100 * 1000,},
+        };
+
+        public static ICollection<MetricUnit> Units { get => units; }
+
+        /// <summary>
+        /// ヤード単位からメートル単位に変換します
+        /// </summary>
+        /// <param name="unit">変換元の単位</param>
+        /// <param name="value">変換する値</param>
+        /// <returns>変換した値</returns>
+        public double FormImperialUnit(ImperialUnit unit,double value) {
+            return (value * unit.Coefficent) * 25.4 / this.Coefficent; 
+        }
+    }
+}
