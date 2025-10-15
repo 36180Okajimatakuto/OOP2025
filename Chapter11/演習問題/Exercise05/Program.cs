24e682f1b6ec7aba9260c026146ea684ea857d99
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-10-15 12:08:02 +0900

    ObserbleCollectionまで

--- Chapter11/演習問題/Exercise05/Program.cs
+++ Chapter11/演習問題/Exercise05/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Exercise05 {
@@ -6,17 +8,19 @@ namespace Exercise05 {
         static void Main(string[] args) {
             var lines = File.ReadLines("sample.html");
             var sb = new StringBuilder();
+
             foreach (var line in lines) {
-                var s = Regex.Replace(line, @"<(/?)([A-Z][A-Z0-9]*)(.*)>",
-                    m => {
-                        string.Format(m.Groups[1].Value,
-                            m.Groups[2].Value.ToLower(),
-                            m.Groups[3].Value)}
-                        "<$1$2$3">};
+                // タグ名を小文字に変換
+                var s = Regex.Replace(line, @"<(/?)([A-Z][A-Z0-9]*)([^>]*)>", m => {
+                    string slash = m.Groups[1].Value;
+                    string tagName = m.Groups[2].Value.ToLower();
+                    string rest = m.Groups[3].Value;
+                    return $"<{slash}{tagName}{rest}>";
+                });
 
-
-
+                sb.AppendLine(s);
             }
+
             File.WriteAllText("sampleOut.html", sb.ToString());
         }
     }
---
+++ SQLite/Sample/Data/Person.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sample.Data
+{
+    class Person
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; }
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; set; } = String.Empty;
+        /// <summary>
+        /// 電話番号
+        /// </summary>
+        public string Phone { get; set; } = String.Empty;
+
+        public override string ToString() {
+            return $"{Id}{Name}{Phone}";
+        }
+    }
+}
