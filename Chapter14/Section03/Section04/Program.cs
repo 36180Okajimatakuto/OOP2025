6baba24858e2708c217f28db7cb3a0456f2f0144
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-11-05 11:16:49 +0900

    コード14.13

--- Chapter14/Section03/Section04/Program.cs
+++ Chapter14/Section03/Section04/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace Section04 {
     internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+        static async Task Main(string[] args) {
+            HttpClient hc = new HttpClient();
+            await GetHtmlExample(hc);
+        }
+
+        static async Task GetHtmlExample(HttpClient httpCliant) {
+            var url = "https://chatgpt.com";
+            var text = await httpCliant.GetStreamAsync(url);
+            Console.WriteLine(text);
         }
     }
 }
