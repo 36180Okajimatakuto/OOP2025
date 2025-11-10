5a6e331aa71c56180f7b840dd0c8394e6e598c92
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-11-10 14:32:22 +0900

    10.12

--- Chapter10/Section01/Program.cs
+++ Chapter10/Section01/Program.cs
@@ -5,18 +5,16 @@ namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
 
-            //10.11　既存テキストファイルの末尾に行を追加する
-            var lines = new[] { "===", "京の夢", "大阪の夢", };
-            var filePath = "./Example/いろは歌.txt";
-            using (var writer = new StreamWriter(filePath, append: true)) {
-                foreach (var line in lines) {
-                    writer.WriteLine(line);
+            //10.12　文字列の配列を一気にファイルに出力する
+            var lines = new[] { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", };
+            var filePath = "./Example/Cities.txt";
+            File.WriteAllLines(filePath,lines);
 
-                }
-            }
         }
     }
 }
+
+
 
 
 
