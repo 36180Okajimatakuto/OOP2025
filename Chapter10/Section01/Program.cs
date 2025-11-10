83e7c99f7354d294e993effab9ccf9a878f97aa6
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-11-10 14:17:08 +0900

    10.4~9まで

--- Chapter10/Section01/Program.cs
+++ Chapter10/Section01/Program.cs
@@ -4,10 +4,37 @@ using System.Text;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            //テキストファイルをIEbumerable<string>として扱う
+
             var filePath = "./Greeting.txt";
-            var lines = File.ReadLines(filePath);
-            foreach (var line in lines) {
+   
+            //10.4 先頭のN行を取り出す
+            var lines = File.ReadLines(filePath)
+                .Take(10)
+                .ToArray();
+
+            //10.5　条件に一致した行数をカウントする
+            var count = File.ReadLines(filePath).Count(s=>s.Contains("C#"));
+
+            //10.6　条件に一致した行だけを取り出す
+            var liness = File.ReadLines(filePath)
+                .Where(s=> !String.IsNullOrWhiteSpace(s))
+            .ToArray();
+
+            //10.7　条件に一致した行が存在しているか調べる
+            var exists = File.ReadLines(filePath)
+                .Where(s=> !String.IsNullOrEmpty(s))
+                .Any(s=> s.All(c=>Char.IsDigit(c)));
+
+            //10.8　重複行を取り除き並べ替える
+            var linesss = File.ReadLines(filePath)
+                .Distinct()
+                .OrderBy(s=> s.Length)
+                .ToArray();
+
+            //10.9　行ごとに何らかの変換処理をする
+            var linessss = File.ReadLines(filePath)
+                .Select((s,ix) => $"(ix + 1,4): {s}");
+            foreach (var line  in lines) {
                 Console.WriteLine(line);
 
             }
