77a00ac0e0891dcd4c4aae493a3a57d81f8dd9b3
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-11-10 14:22:09 +0900

    10.10　テキストファイルに一行ずつ文字列を出力する

--- Chapter10/Section01/Program.cs
+++ Chapter10/Section01/Program.cs
@@ -3,40 +3,13 @@ using System.Text;
 
 namespace Section01 {
     internal class Program {
-        static void Main(string[] args) {
-
-            var filePath = "./Greeting.txt";
-   
-            //10.4 先頭のN行を取り出す
-            var lines = File.ReadLines(filePath)
-                .Take(10)
-                .ToArray();
-
-            //10.5　条件に一致した行数をカウントする
-            var count = File.ReadLines(filePath).Count(s=>s.Contains("C#"));
-
-            //10.6　条件に一致した行だけを取り出す
-            var liness = File.ReadLines(filePath)
-                .Where(s=> !String.IsNullOrWhiteSpace(s))
-            .ToArray();
-
-            //10.7　条件に一致した行が存在しているか調べる
-            var exists = File.ReadLines(filePath)
-                .Where(s=> !String.IsNullOrEmpty(s))
-                .Any(s=> s.All(c=>Char.IsDigit(c)));
-
-            //10.8　重複行を取り除き並べ替える
-            var linesss = File.ReadLines(filePath)
-                .Distinct()
-                .OrderBy(s=> s.Length)
-                .ToArray();
-
-            //10.9　行ごとに何らかの変換処理をする
-            var linessss = File.ReadLines(filePath)
-                .Select((s,ix) => $"(ix + 1,4): {s}");
-            foreach (var line  in lines) {
-                Console.WriteLine(line);
-
+        static void Main(string[] args) {//10.10　テキストファイルに一行ずつ文字列を出力する
+            var filePath = "./Example/いろは歌.txt";
+            using (var writer = new StreamWriter(filePath)) {
+                writer.WriteLine("色はにほへど　散りぬるを");
+                writer.WriteLine("我が世たれぞ　常ならむ)");
+                writer.WriteLine("有為の奥山　今日越えて");
+                writer.WriteLine("浅き夢見じ　酢ひもせず");
             }
         }
     }
