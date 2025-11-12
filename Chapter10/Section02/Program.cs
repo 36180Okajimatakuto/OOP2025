db2f8b9c1f6498bd2edaa46d7eef58332d57ba3d
36180Okajimatakuto <oibc36180@ojs.ac.jp>
2025-11-12 10:20:43 +0900

    インターフェイスを使ったポリモーフィズム

--- Chapter10/Section02/Program.cs
+++ Chapter10/Section02/Program.cs
@@ -2,7 +2,7 @@
     internal class Program {
         static void Main(string[] args) {
 
-
+            //10.14ファイルの先頭に行を挿入する
             var filePath = "./Example/いろは歌.txt";
             using var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
             using var reader = new StreamReader(stream);
---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<IGreeting> Greetings = [
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+                ];
+
+            foreach (var obj in Greetings) {
+                string msg = obj.GetMessage();
+                Console.WriteLine(msg);
+            }
+        }
+        class GreetingMorning : IGreeting {
+            public  string GetMessage() => "おはよう";
+        }
+        class GreetingAfternoon : IGreeting {
+            public  string GetMessage() => "こんにちは";
+        }
+        class GreetingEvening : IGreeting {
+            public  string GetMessage() => "こんばんは";
+        }
+    }
+}
